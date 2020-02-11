9ae63cc64f1c01d15faf7b4dadd852b911c7168e
shantix <shantix@spaceofjoy.ru>
2020-02-11 18:51:42 +0300

    Js: Create & manipulate with inputs

---
+++ script.js
@@ -0,0 +1,20 @@
+// document.querySelector("#ranger").oninput = () => {
+//     let take_range = document.querySelector("#ranger").value;
+//     document.querySelector("#inputer").innerHTML = take_range;
+// };
+
+
+document.querySelector("#btn").onclick = () => {
+    let get_check = document.querySelector("#ch1");
+    if (get_check.checked) {
+        console.log("Checked");
+    } else {
+        console.log("Not checked");
+    }
+
+    let former = document.querySelector('#former');
+    console.log(former.elements.)
+}
+
+
+
