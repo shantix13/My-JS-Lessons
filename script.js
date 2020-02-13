58aed99605f6a01c8f32eff27d7dd9f5d566c855
shantix <shantix@spaceofjoy.ru>
2020-02-13 17:35:05 +0300

    Associated Arrays

--- script.js
+++ script.js
@@ -1,62 +1,28 @@
-let my_array = [4, 5, 6, 7, 8, 9];
+const associate_arr = {}; // инициализируем массив
 
-// Создаем двумерный массив. Обрати внимание на стиль написания.
+let associate_arr_2 = {
+    'a': 5,
+    'b': 'Hello',
+    'z2': 'Hi'
+}
 
-let multi_array = [
-    [1, 2],
-    [4, 5],
-    [6, 7]
-];
+// Ключ: значение 
+// Добавим элемент массива
+associate_arr_2.yyy = 555;
+associate_arr_2.z2 = 444;
+delete associate_arr_2.b;
+delete associate_arr_2['a'];
+console.log(associate_arr_2);
+//console.log(associate_arr_2.z2);
 
-//console.log(multi_array);
+// Если имя переменной, то лучше писать так
+// let k = 'z2';
+// console.log(associate_arr_2[k]);
 
-// for (let i = 0; i < multi_array.length; i++) {
-//     //console.log(multi_array[i]);
-//     let c = multi_array[i]; // для упрощения можно сделать так.
-//     for (let k = 0; k < c.length; k++) {
-//         console.log(c[k]);
-//     }
-// }
-
-// for (let i = 0; i < multi_array.length; i++) {
-//     //console.log(multi_array[i]);
-//     let c = multi_array[i]; // для упрощения можно сделать так.
-//     for (let k = c.length - 1; k >= 0; k--) {
-//         console.log(c[k]);
-//     }
-// }
-
+// цикл for in для ассоциативных массивов
 let out = '';
-
-// for (let i = 0; i < multi_array.length; i++) {
-//     for (let k = 0; k < multi_array[i].length; k++) {
-//         out += multi_array[i][k] + ' ';
-//     }
-//     out += '<br>';
-// }
-
-// document.querySelector('.out').innerHTML = out;
-
-for (let i = 0; i < multi_array.length; i++) {
-    for (let k = 0; k < multi_array[i].length; k++) {
-        if (multi_array[i][k] > 4) {
-            out += multi_array[i][k] + ' ';
-        }
-    }
-    out += '<br>';
+for (let key in associate_arr_2) {
+    out += associate_arr_2[key];
 }
 
 document.querySelector('.out').innerHTML = out;
-
-
-let new_array = [1, 0, 0, 0, 0, 0, 0];
-let incr = 0;
-document.querySelector('.out2').innerHTML = new_array;
-document.querySelector('button').onclick = function () {
-    if (incr + 1 < new_array.length) {
-        new_array[incr] = 0;
-        new_array[incr + 1] = 1;
-        incr++;
-    }
-    document.querySelector('.out2').innerHTML = new_array;
-}
