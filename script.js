40d4cfd9b42a4cd4dfacb65e472fc5f58ca46c34
shantix <shantix@spaceofjoy.ru>
2020-02-20 15:44:01 +0300

    JS: keyboard events

--- script.js
+++ script.js
@@ -1,46 +1,29 @@
-document.querySelector('.one').onclick = (event) => {
-    console.log(e);
-    console.log('click-one');
-}
 
-document.querySelector('.two').ondblclick = (e) => {
-    console.log('double-click');
-}
+// событие изменения input
+// document.querySelector('.inputer').oninput = (event) => {
+//     console.log(event);
+// }
+
+// при нажатии клавиши в поле input
+// document.querySelector('.inputer').onkeypress = (event) => {
+//     // console.log(event); keyboard event
+//     console.log(event.charCode);
+//     console.log(event.code);
+//     console.log(event.keyCode);
+// }
+
+// 48-57 code - это цифры
+
+// вниз и вверх фиксируется только на клавишах. Вспомогательные
+// клавиши не обрабатываются методом onkeypress.
+// Нужно использовать onkeyup или onkeydown
 
 
-// mousemove не работает попиксельно.
-document.querySelector('.three').onmousemove = () => {
-    console.log('mousemove');
-}
-
-
-document.querySelector('.three').onmouseenter = () => {
-    document.querySelector('.three').style.background = 'red';
-}
-
-document.querySelector('.three').onmouseleave = function () {
-    document.querySelector('.three').style.background = 'green';
-}
-
-document.querySelector('.three').onmousedown = () => {
-    document.querySelector('.three').style.background = 'yellow';
-
-}
-
-document.querySelector('.three').onmouseup = () => {
-    document.querySelector('.three').style.background = 'orange';
-
+document.querySelector('.inputer').onkeydown = (event) => {
+    // console.log(event); keyboard event
+    console.log(event.charCode);
+    console.log(event.code);
+    console.log(event.keyCode);
 }
 
 
-document.querySelector('.one').oncontextmenu = () => {
-    console.log('contextmenu');
-    return false;
-}
-
-
-let prog_bar = 10;
-document.querySelector('button').onclick = () => {
-    prog_bar++;
-    document.querySelector('progress').value = prog_bar;
-}
