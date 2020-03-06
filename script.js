5e941e3b4e896e98cd722d85c6887fb4abf3eaae
shantix <shantix@spaceofjoy.ru>
2020-03-06 11:47:22 +0300

    Work with LocalStorage

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
-let out1 = 0;
+// localStorage.setItem('data', 560);
+// let store = localStorage.getItem('data');
+// console.log(store);
 
-document.querySelector('.block-1').onclick = () => {
-    out1++;
-    document.querySelector('.out-1').innerHTML = 'click' + out1;
-}
+const arr_storage = [3, 4, 5];
 
+localStorage.setItem('my_arr', arr_storage);
 
-let out2 = 0;
+let new_arr = localStorage.getItem('my_arr');
 
-document.querySelector('.block-2').ondblclick = () => {
-    out2++;
-    document.querySelector('.out-2').innerHTML = 'dblclick' + out2;
-}
+console.log(new_arr);
 
-
-let out3 = 0;
-
-document.querySelector('.block-3').onmousemove = () => {
-    out3++;
-    document.querySelector('.out-3').innerHTML = 'mousemove' + out3;
-}
-
-let out4 = 0;
-
-document.querySelector('.block-4').onmousenter = () => {
-    out4++;
-    document.querySelector('.out-4').innerHTML = 'mouseenter' + out4;
-}
-
-let out5 = 0;
-
-document.querySelector('.block-5').onmouseleave = () => {
-    out5++;
-    document.querySelector('.out-5').innerHTML = 'mouseleave' + out5;
-} 
+// При получении массива из localStorage массив преобразуется в строку. Для получени массива надо использовать JSON
