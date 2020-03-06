4b0ffeb405b95f160ff66e0b87614ba215d36978
shantix <shantix@spaceofjoy.ru>
2020-03-06 11:27:37 +0300

    No touch events on phones Android & IOS

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-document.querySelector('.block-1').addEventListener('touchstart', myTouch);
-document.querySelector('.block-1').addEventListener('touchend', myTouchEnd);
-document.querySelector('.block-2').addEventListener('touchmove', myTouchMove);
+let out1 = 0;
 
-function myTouch(event) {
-    // console.log('touch');
-    // console.log(event);
-    document.querySelector('.out-1').innerHTML = event.touches.length;
-}
-
-// событие touch не работает на компе, но можно эмулировать его в chrome 
-// web dev tool
-
-// в событии event:
-// touchlist - сколько раз 
-// force - интенсивность нажатия
-// target - элемент на котором происходит событие
-// radius - радиус события (захвачен ли элемент или нет)
-
-function myTouchEnd(event) {
-    document.querySelector('.out-2').innerHTML += 'end';
-}
-
-function myTouchMove(e) {
-    event.preventDefault();
-    document.querySelector('.block-2').innerHTML += 'move';
-    return false;
+document.querySelector('.block-1').onclick = () => {
+    out1++;
+    document.querySelector('.out-1').innerHTML = 'click' + out1;
 }
 
 
+let out2 = 0;
+
+document.querySelector('.block-2').ondblclick = () => {
+    out2++;
+    document.querySelector('.out-2').innerHTML = 'dblclick' + out2;
+}
 
 
+let out3 = 0;
+
+document.querySelector('.block-3').onmousemove = () => {
+    out3++;
+    document.querySelector('.out-3').innerHTML = 'mousemove' + out3;
+}
+
+let out4 = 0;
+
+document.querySelector('.block-4').onmousenter = () => {
+    out4++;
+    document.querySelector('.out-4').innerHTML = 'mouseenter' + out4;
+}
+
+let out5 = 0;
+
+document.querySelector('.block-5').onmouseleave = () => {
+    out5++;
+    document.querySelector('.out-5').innerHTML = 'mouseleave' + out5;
+} 
