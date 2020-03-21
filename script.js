002448aa930af813b7f71148c3a8a347fdafdea7
shantix <shantix@spaceofjoy.ru>
2020-03-21 10:57:55 +0300

    ООП в JS: part 1

--- script.js
+++ script.js
@@ -1,63 +1,46 @@
-// // Послыает запросы на http ресурсы
-// let new_request = new XMLHttpRequest();
+function myAlert(a, c, d) {
+    //let a = 'Error';
+}
 
-// function myFucntion(data) {
-//     console.log(data);
-// }
+myAlert('Error', 'red', '.test2');
 
-// new_request.open('GET', 'http://getpost.itgid.info/index2.php', true); // либо true либо false
-// new_request.send();
+class Alert {
+    constructor(a, c, d) {
+        this.message = a;
+        this.cssClass = c;
+        this.out = d;
+    }
 
-// new_request.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//         myFucntion(this.responseText)
-//     }
-// }
-
-
-// const http2 = new XMLHttpRequest();
-
-// http2.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//         myFucntion2(this.responseText)
-//     }
-// }
-
-// http2.open('POST', 'http://getpost.itgid.info/index2.php', true); // либо true либо false
-// hhtp2.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-// http2.send();
-
-// Fetc всегда возвращает promise
-fetch('http://getpost.itgid.info/index2.php')
-    .then(data => {
-        console.log(data);
-        console.log(data.text());
-        data.text().then(data2 => {
-            console.log(data2);
-        })
-            .then(data => {
-                console.log(data);
-            })
-    });
-
-// POST REQUEST 
-
-fetch('http://getpost.itgid.info/index2.php', {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-        //'Content-Type': 'application/json'
-        'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *client
-    body: 'auth=password' // body data type must match "Content-Type" header
-})
-    .then(response => response.text())
-    .then(response => {
-        console.log(response);
-    })
+    showAlert() {
+        document.querySelector(this.out).innerHTML = `<p class="${this.cssClass}">${this.message}</p>`;
+    }
+}
 
 
+let m = new Alert('My Message', 'orange', '.test');
+
+
+class Alert2 extends Alert {
+    constructor(a, c, d, icon) {
+        super(a, c, d);
+        this.icon = icon;
+    }
+
+    showIconAlert() {
+        document.querySelector(this.out).innerHTML = `<p class="${this.cssClass}"><i class="material-icons">${this.icon}</i>${this.message}</p>`;
+    }
+
+    myAlert() {
+        alert(this.message);
+    }
+}
+
+console.log(m);
+
+m.showAlert();
+
+let m2 = new Alert2('My Message', 'red', '.test', 'account_balance');
+
+m2.showIconAlert();
+
+m2.myAlert(this.message);
