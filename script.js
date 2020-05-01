f636fe8c8841c777e5cdb7994f60cb32fe303a49
shantix <shantix@spaceofjoy.ru>
2020-05-01 14:22:01 +0300

    Js: Больше про перебор массивов

--- script.js
+++ script.js
@@ -1,31 +1,81 @@
-// onclick
-
-// document.querySelector('#btn').onclick = e => {
-//     console.log(e);
-// };
-
-// Variable better
-
-const clicker = document.querySelector('#btn');
-// oncliсk перезаписывает событие
-// clicker.onclick = e => console.log(e);
+let array = [
+    { name: 'Игорь', age: 49, budget: 50000 },
+    { name: 'Антон', age: 44, budget: 40000 },
+    { name: 'Анна', age: 40, budget: 44000 },
+    { name: 'Вероника', age: 35, budget: 20000 }
+];
 
 
-// addEventListener выполняет все задекларированные события.
-clicker.addEventListener('click', e => {
-    console.log(e.target.tagName);
-});
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
 
 
-clicker.addEventListener('mouseout', myFunc);
-
-function myFunc() {
-    alert('Hello');
-}
-
-// отличия target и currentTarget
-// target - event будет сам элемент, на который мы нажимаем. вложенный элемент.
-// currentTarget - элемент, который  является родителем нажимаего элемента.
+// for (person of array) {
+//     console.log(person);
+// }
 
 
+// метод forEach
 
+// array.forEach(function (person, index, arrayx) {
+//     console.log(person);
+// });
+
+// array.forEach(person => console.log(person));
+
+
+// Метод map
+// const newarray = array.map(person => {
+//     return `${person.name} (${person.age})`;
+// });
+
+// const newarray = array.map(person => `${person.name} (${person.age})`);
+
+// Метод filter
+
+// const newarray = array.filter(person => {
+//     if (person.age > 39) {
+//         return person;
+//     }
+// });
+
+// const newarray = array.filter(person => {
+//     return person.age > 39;
+// });
+
+// const newarray = array.filter(person => person.age > 39);
+
+// Метод reduce
+
+// const newarray = array.reduce(function (newB, oldB) {
+//     return newB + oldB.budget;
+// }, 0);
+
+// const newarray = array.reduce((totalx, personx) => {
+//     return totalx + personx.budget;
+// }, 0)
+
+// const newarray = array.reduce((totalx, personx) => totalx + personx.budget, 0);
+
+
+// Метод find 
+
+// const newarray = array.find(personx => personx.name == "Игорь");
+
+// метод findIndex
+
+// const newarray = array.findIndex(personx => personx.name == "Игорь");
+
+
+// console.log(newarray);
+
+const maxBudget = array.filter(personx => personx.budget > 30000).map(personx => {
+    return {
+        info: `${personx.name}(${personx.age})`,
+        budget: personx.budget,
+    }
+}).
+    reduce((totalx, budgetx) => totalx + budgetx.budget, 0);
+
+console.log(maxBudget);
