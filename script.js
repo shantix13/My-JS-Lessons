78ef888fdc9fc933b4ad42bf5ea538b33131e00f
shantix <shantix@spaceofjoy.ru>
2020-06-22 10:28:11 +0300

    More of a promise

--- script.js
+++ script.js
@@ -1,38 +1,13 @@
-const b1 = document.querySelector('.b-1');
-const b2 = document.querySelector('.b-2');
+let a = 7;
 
 
-//b1.onclick = test;
+setTimeout(() => { a = 22 },
+    99);
 
-function test(color, num) {
-    console.log(num);
-    this.style.background = color;
-}
+console.log(a);
 
-//test();
-
-// метод call();
-
-// function.call(context, [arg1, arg2, arg3....])
-
-
-//test.call(b1); // сразу вызвал функцию - this = b1
-
-// b2.onclick = function () {
-//     test.call(b1, 'green');
-// }
-
-
-// apply
-// b2.onclick = function () {
-//     test.apply(b1, ['green', 555]);
-// }
-
-// bind
-
-let a = test.bind(b1, 'orange', 6666);
-let b = test.bind(b2, 'green', 8888);
-
-b2.onclick = a;
-b1.onclick = b;
-
+let b = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve(a = 99);
+    }, 2000);
+});
